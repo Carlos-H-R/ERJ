771a1ccca7551fbb1012beeb033d21c9d0124502
Carlos-H-R <116239140+Carlos-H-R@users.noreply.github.com>
2023-06-07 14:54:57 -0300

    Update joanaMMC.cpp

--- DIA/Aula 9/Joana e o MMC/joanaMMC.cpp
+++ DIA/Aula 9/Joana e o MMC/joanaMMC.cpp
@@ -1,6 +1,56 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+int C[31700], P[4000001], rq, nf, np, nd;
+long long int n, q, F[50], D[10000];
+bool teste;
+
+void GeraCrivo (int n)
+{    int i, rq, t, d;
+     for (i=1; i<=n; i++)   C[i] = i;
+     for (i=2; i<=n; i+=2)  C[i] = 2;
+     rq = sqrt(n+0.5);
+     for (i=3; i<=rq; i+=2)
+     {   if (C[i] == i)
+         {   t = i*i;  d = i+i;
+             while(t<=n)
+             {   if (C[t] == t) C[t] = i;
+                 t = t+d;
+             }
+         }
+     }
+}
+void GeraPrimos (int rq)
+{    np=0;
+     for (int i=2; i<=rq; i++)
+         if (C[i]==i)  P[++np]= i;
+}
+
+void Fatora (long long int q)
+{    int rq = sqrt(q+0.5);
+	 nf = 0; 
+     for (int i=1; i <=np; i++)
+     {   while((q % P[i]) == 0)
+         {   F[++nf] = P[i];  q = q/P[i];
+         }
+         if (q == 1 || P[i] >= rq) break;  
+     }
+     if (q != 1) F[++nf] = q;
+}
+
+void Divisores(long long int q)
+{    int nda;long long int k;
+     F[0] = 1;  nd = 1;  D[1] = 1;  nda = 1;
+     for (int i=1; i<=nf; i++)
+     {   if (F[i] == F[i-1]) k = k*F[i];
+         else
+         {    nda = nd;  k = F[i];
+         }
+         for (int j=1; j<=nda; j++)
+             D[++nd] = k*D[j];
+     }
+}
 
 int MDC(int n1, int n2){
     // Calcula MDC
@@ -13,12 +63,19 @@ int main() {
     
     cin >> t;
 
+    GeraCrivo(31700);
+    GeraPrimos(31700);
+
+
     for (int i=0; i<t; i++){
         cin >> N;
 
-        // pode testar usar a lista de divisores
+        Fatora(N);
+        Divisores(N);
 
-        // pode testar todos os numeros
+        if ((nd % 2) == 0) (cout << "Bruno");
+
+        else (cout << "Joana");
     }
 
     return 0;
