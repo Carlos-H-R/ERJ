aad6bc97e32a24f3c7d5484c5bd00dfc88564e01
Carlos-H-R <carlos.ribas.chr@gmail.com>
2023-09-24 21:32:09 -0300

    update Calculo Numerico

---
+++ CN/Metodos/EliminacaoGaussiana.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+def egm(A,b,m,n):
+    aux=[]
+    for i in range(n):
+        id_aux = np.identity(m)
+        for j in range(i+1,m):
+            id_aux[j,i] = -A[j,i]/A[i,i]
+        aux.append(id_aux)
+    # Ab = np.array(aux)
+
+    Ab = A
+    bb = b
+    for mx in aux:
+        Ab = mx@Ab
+        bb = mx@bb
+
+
+    return Ab,bb,aux
+
+def mult(X):
+    x = 1
+    for i in X:
+        for j in i:
+            x *= j
+    return x
+
+def e_gauss(A,b, m, n):
+    mb = 1
+    nb = n
+    Ab = np.block([A,b])
+    for i in range(1,m):
+        for j in range(i,m):
+            line_multi = Ab[j,i-1]/Ab[i-1,i-1]
+            for k in range(0,m+1):
+                Ab[j,k] = Ab[j,k] - line_multi*Ab[i-1,k]
+    T = np.triu(Ab[:,0:n])
+    y = Ab[:,n:n+1]
+    return T,y
+
+A = []
+m,n = map(int, input().split())
+for i in range(m):
+    A.append(list(map(float,input().split())))
+A = np.array(A)
+
+aux = list(map(float,input().split()))
+b = []
+for i in aux:
+    b.append([i])
+b = np.array(b)
+
+
+
+
+print(A)
+print(b)
+# [U, bu] = e_gauss(A,b,m,n)
+[U,bu,aux] = egm(A,b,m,n)
+print('U =\n',U)
+print('bu=\n',bu)
+Y = np.linalg.solve(U,bu)
+print('Soma Y = ', sum(Y))
+print('Prod Y = ', mult(Y))
+
+for i in range(len(aux)):
+    print('A%d =\n'%(i+1),aux[i])
