deb5c0ae24a710f5aefb5cea962bbd27ba7fc2e3
Carlos-H-R <carlos.ribas.chr@gmail.com>
2023-11-27 22:45:46 -0300

    update Calculo Numerico

--- CN/Metodos/Integral_Num_Simp.py
+++ CN/Metodos/Integral_Num_Simp.py
@@ -1,4 +1,13 @@
 import numpy as np
+# array = [0.993252, 1.202972, 1.376244, 1.52388, 1.652497, 1.766442, 1.868721]
+
+def error(found, original):
+    if original != 0:
+        return (abs(found - original)/abs(original))
+    
+    else:
+        print("\nDivisao por Zero!\n")
+        return np.NAN
 
 
 def simp_1_3(f,a,b,n):
@@ -23,21 +32,52 @@ def simp_1_3(f,a,b,n):
     return integral
 
 
+def simpson_1_3(f, a, b, n):
+    """
+    Calcula a integral definida de uma função f(x) no intervalo [a, b] usando o método 1/3 de Simpson.
+
+    :param f: A função a ser integrada.
+    :param a: O limite inferior do intervalo.
+    :param b: O limite superior do intervalo.
+    :param n: O número de subintervalos. Deve ser um número par.
+    :return: O valor da integral aproximada ou NaN em caso de erro.
+    """
+    try:
+        if n % 2 != 0:
+            raise ValueError("O número de subintervalos (n) deve ser par.")
+
+        h = (b - a) / n
+        x = [a + i * h for i in range(n + 1)]
+        y = [f(xi) for xi in x]
+
+        integral = y[0] + 4 * sum(y[i] for i in range(1, n, 2)) + 2 * sum(y[i] for i in range(2, n - 1, 2)) + y[n]
+        integral *= h / 3
+
+        return integral
+    except Exception as e:
+        print(f"Erro: {str(e)}")
+        return float(np.nan)
+
+
 def simp_1_3_New():
     pass
 
 
 def f(t):
-    return t**3
+    return ((t**2))
 
 
 # Input
 a = float(input())
 b = float(input())
 n = int(input())
-result = simp_1_3(f,a,b,n)
+result = simpson_1_3(f,a,b,n)
 
 
 # Output
-np.set_printoptions(precision=4)
+np.set_printoptions(precision=5)
 print("Integral = ",result)
+
+# # Erro Relativo
+# original = 1.6488 # <-- Colocar aqui o valor original
+# print("Erro Relativo = ",error(result,original))
--- CN/Metodos/Integral_Num_Trap.py
+++ CN/Metodos/Integral_Num_Trap.py
@@ -1,13 +1,23 @@
 import numpy as np
+# array = [0.961, 0.996, 0.995, 0.958, 0.887, 0.784, 0.653]
+
+def error(found, original):
+    if original != 0:
+        return (abs(found - original)/abs(original))
+    
+    else:
+        print("\nDivisao por Zero!\n")
+        return np.NAN
 
 
 def integral_trap(f,a,b,n):
     h = (b - a)/n
-    integral = f(a) + f(b)
+    integral = f(a)
 
     for i in range(1,n):
         integral += 2 * f(a+(h*i))
     
+    integral += f(b)
     integral *= h/2
 
     return integral
@@ -18,7 +28,7 @@ def integral_trap_tab(f,x,y):
         n = len(x)    
 
         h = x[1] - x[0]
-        integral = y[0] + y[n]
+        integral = y[0] + y[-1]
 
         for i in range(1,n-1):
             integral += 2 * y[i]
@@ -32,7 +42,7 @@ def integral_trap_tab(f,x,y):
 
 
 def f(t):
-    return t**3
+    return ((t ** 3))
 
 
 # # Input Tabelado
@@ -47,5 +57,10 @@ n = int(input())
 result = integral_trap(f,a,b,n)
 
 # Output
-np.set_printoptions(precision=4)
+np.set_printoptions(precision=5)
 print("Integral = ",result)
+
+
+# # Erro Relativo
+# original = 1.6488 # <-- Colocar aqui o valor original
+# print("Erro Relativo = ",error(result,original))
