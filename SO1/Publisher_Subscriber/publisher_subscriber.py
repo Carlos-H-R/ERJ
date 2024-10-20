e965587221140e3c2b21ce71ca7aff14dcdd6bd5
Carlos-H-R <carlos.ribas.chr@gmail.com>
2024-10-20 15:45:17 -0300

    Criada pasta para codigos de SO1

---
+++ SO1/Publisher_Subscriber/publisher_subscriber.py
@@ -0,0 +1,21 @@
+import threading
+import queue
+
+
+def publisher():
+    # Envia mensagens para tópicos específicos
+    pass
+
+
+def subscriber():
+    # 
+    pass
+
+
+def broker():
+    pass
+
+
+
+if __name__ == "__main__":
+    pass
